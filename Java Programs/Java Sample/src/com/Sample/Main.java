6b196b01b598f4e6274a9d0340b75f673da1a099
its_hacker_official <82884717+its-hacker-official@users.noreply.github.com>
2022-07-29 10:13:17 +0530

    Add files via upload

---
+++ Java Programs/Java Sample/src/com/Sample/Main.java
@@ -0,0 +1,18 @@
+package com.Sample;
+
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int[] number = {1,2,3,4,5};
+
+
+
+
+
+    }
+}
---
+++ Java Programs/JavaPractice 1/src/com/AgeColculation/Main.java
@@ -0,0 +1,41 @@
+package com.AgeColculation;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+       String i;
+       int j;
+
+       Scanner input = new Scanner(System.in);
+        System.out.println("Enter Your name: ");
+        i = input.next();
+
+        if(i=="naveen") {
+
+             System.out.println("He is bad boy");
+         }
+
+        else if(i=="pandi") {
+
+             System.out.println("He is good boy");
+         }
+
+        else{
+            System.out.println("Invalid name");
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
